6afbd6b9dd70b663e0f92390db6d2937084ed88c
Marco Kalf <marco.kalf@gmx.net>
2018-06-27 20:01:55 +0200

    Fehlende Funktionen hinzugefügt

---
+++ Rune/Inventory.gd
@@ -0,0 +1,10 @@
+extends "res://Rune/RuneSlots.gd"
+
+func _ready():
+	pass
+
+func _on_RuneSlot_slot_changed():
+	pass
+	
+func get_runes():
+	pass
--- Rune/RuneSlots.gd
+++ Rune/RuneSlots.gd
@@ -3,11 +3,16 @@ extends Control
 signal slot_changed;
 
 func _ready():
-
+	connect('slot_changed',get_parent(),'runes_changed')
 	pass
 
 func _on_RuneSlot_slot_changed():
+	emit_signal('slot_changed')
 	pass
 	
 func get_runes():
-	pass
+	var runes = [] 
+	for slot in $VBoxContainer/HBoxContainer.get_children():
+		for stone in slot.get_children():
+			runes.push_back(stone.rune.instance())
+	return runes
