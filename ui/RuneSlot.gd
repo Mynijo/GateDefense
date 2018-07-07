958cd7c5c396bccbbdbcbc478cb4ecb1f75d3b95
Marco Kalf <marco.kalf@gmx.net>
2018-07-07 13:29:22 +0200

    missing file

--- ui/RuneSlot.gd
+++ ui/RuneSlot.gd
@@ -9,10 +9,14 @@ func can_drop_data(position, data):
 	return true
 	
 func drop_data(position, data):
-	remove_rune(data)	
+	if(data.get_parent().has_method('remove_rune')):
+		remove_rune(data)
+	else:
+		data.get_parent().remove_child(data)	
 	add_child(data)
 	emit_signal('slot_changed')
 func remove_rune(rune):
+	rune.get_parent().emit_signal('slot_changed')
 	rune.get_parent().remove_child(rune)
-	emit_signal('slot_changed')
+	#emit_signal('slot_changed')
 	
